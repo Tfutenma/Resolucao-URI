cefd8fc7f4448a12fb7bb1627c4473e4985352f0
Thiago T. Futenma <tfutenma@hotmail.com>
2020-11-17 20:53:02 -0300

    12, 13, 14 em C

---
+++ C/Starter/1013.c
@@ -0,0 +1,28 @@
+/*
+Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+MaiorAB = (a+b+abs(a-b))/2
+
+
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+Entrada
+O arquivo de entrada contém três valores inteiros.
+
+Saída
+Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+      int a, b, c;
+      scanf("%d %d %d", &a, &b, &c);
+      int maior;
+      maior = (a + b + abs(a - b)) / 2;
+      maior = (maior + c + abs(maior - c)) / 2;
+      printf("%d eh o maior\n", maior);
+
+      return 0;
+}
